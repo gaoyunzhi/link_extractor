49e36de4939cadc2abda2f4d5cb2e178dd38ad83
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-11 08:43:25 -0700

    test

--- test.py
+++ test.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import link_extractor
-import os
-import sys
 
 def test():
-	# link_extractor.gen(news_source='bbc')
-	pdf_name = link_extractor.gen(news_source='nyt英文')
-	os.system('open %s -g' % pdf_name)
+	links = link_extractor.getLinks(
+		'https://www.bbc.com/zhongwen/simp', domain='https://www.bbc.co.uk')
+	print(links)
 	
-test()
+if __name__=='__main__':
+	test()
