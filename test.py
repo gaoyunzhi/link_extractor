5aab06e538868e2dcf05df4d5cc806d368c8a920
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-06-28 20:34:55 -0700

    commit

--- test.py
+++ test.py
@@ -5,13 +5,14 @@ import link_extractor
 
 def test():
 	links = link_extractor.getLinks(
+		'https://squatting2047.com')
 		# 'https://www.bbc.com/zhongwen/simp', domain='https://www.bbc.co.uk')
 		# 'https://cn.nytimes.com')
 		# 'https://www.bbc.co.uk')
 		# 'https://www.nytimes.com')
 		# 'https://whogovernstw.org')
 		# 'https://www.thinkingtaiwan.com')
-		'https://matters.news/')
+		# 'https://matters.news/')
 	for link, name in links:
 		print(name, link)
 	print(len(links))
