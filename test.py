80bf0f782aa14be685d146c9c44ca39ebaa94f20
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-13 14:18:40 -0700

    commit

--- test.py
+++ test.py
@@ -2,25 +2,33 @@
 # -*- coding: utf-8 -*-
 
 import link_extractor
+import msvcrt as m
+
+tests = [
+	'http://tagesschau.de',
+	'http://www.gzhshoulu.wang/account_PourMarx.html',
+	'https://cn.nytimes.com',
+	'https://jacobinmag.com/',
+	'https://matters.news/@Margaux1848',
+	'https://matters.news/@zoezhao',
+	'https://squatting2047.com',
+	'https://wemp.app/accounts/12c2c49c-305d-4de3-85c8-00d372e7a47a',
+	'https://www.bbc.co.uk',
+	'https://www.bbc.com/zhongwen/simp',
+	'https://www.douban.com/',
+	'https://www.douban.com/explore/',
+	'https://www.douban.com/people/180693708/notes',
+	'https://www.douban.com/people/80620968/notes',
+	'https://www.nytimes.com',
+]
 
 def test():
-	links = link_extractor.getLinks(
-		# 'https://www.douban.com/explore/')
-		'https://www.douban.com')
-		# 'https://www.douban.com/people/180693708/notes')
-		# 'http://www.gzhshoulu.wang/account_PourMarx.html')
-		# 'https://wemp.app/posts/8ee508d2-592d-479d-9d1d-4b76a10e5442')
-		# 'https://squatting2047.com/page/2')
-		# 'https://www.bbc.com/zhongwen/simp', domain='https://www.bbc.co.uk')
-		# 'https://cn.nytimes.com')
-		# 'https://www.bbc.co.uk')
-		# 'https://www.nytimes.com')
-		# 'https://whogovernstw.org')
-		# 'https://www.thinkingtaiwan.com')
-		# 'https://matters.news/')
-	for link, name in links:
-		print(name, link)
-	print(len(links))
+	for site in tests:
+		links = link_extractor.getLinks(site)
+		for link, name in links:
+			print(name, link)
+		print(len(links))
+		m.getch()
 	
 if __name__=='__main__':
 	test()
