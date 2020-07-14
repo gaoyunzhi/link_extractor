c56ced65f7d99d7053aba7839ed1e7eb346a4362
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-13 18:30:18 -0700

    commit

--- test.py
+++ test.py
@@ -24,20 +24,17 @@ tests = [
 ]
 
 def test():
-	fn = 'result1.txt'
-	os.system('rm ' + fn)
 	for site in tests:
 		links = link_extractor.getLinks(site)
 		links.sort()
 		for link, name in links:
-			with open(fn, 'a') as f:
-				f.write(name + ' ' + link + '\n')
-			# print(name, link)
-			...
-		with open(fn, 'a') as f:
-			f.write(str(len(links)) + '\n')
-		# print(len(links))
-		# input()
+			# with open(fn, 'a') as f:
+			# 	f.write(name + ' ' + link + '\n')
+			print(name, link)
+		# with open(fn, 'a') as f:
+		# 	f.write(str(len(links)) + '\n')
+		print(len(links))
+		input()
 	
 if __name__=='__main__':
 	test()
