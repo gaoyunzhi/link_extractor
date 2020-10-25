9b0010774433fdf086556fb2e09fc5fd35a39dec
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-10-25 15:18:10 -0700

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="link_extractor",
-    version="0.0.50",
+    version="0.0.51",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Extract Links from news source, ranked by importance.",
